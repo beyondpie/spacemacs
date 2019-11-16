b23c204b80a025350f5765e3867eb09b9b0fa0cf
beyondpie <zsp07@tsinghua.org.cn>
2019-11-16 12:08:18 -0500

    minor: change to same gc as spacemacs.

--- init.el
+++ init.el
@@ -15,7 +15,7 @@
 ;; Avoid garbage collection during startup.
 ;; see `SPC h . dotspacemacs-gc-cons' for more info
 (defconst emacs-start-time (current-time))
-(setq gc-cons-threshold 202653184 gc-cons-percentage 0.6)
+(setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
 (load (concat (file-name-directory load-file-name)
               "core/core-versions.el")
       nil (not init-file-debug))
